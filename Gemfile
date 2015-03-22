ba1b64dce4825d3d552dacb74d5c930db1161f31
Stephen-Lee <stephen_leez@sina.com>
2015-03-22 20:36:53 +0800

    not finish zh-CN translation

--- Gemfile
+++ Gemfile
@@ -35,6 +35,13 @@ gem 'bootstrap-sass'
 
 gem 'simple_form'
 
+#用户验证
+gem 'devise'
+gem 'devise-i18n'
+
+
+gem 'rails-i18n'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+ 
+ validates :nick_name, presence: true, length: {minimum: 2, maximum: 10},
+          uniqueness: {case_sensitive: false}
+
 end
