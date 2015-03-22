790663b49b2c838afcfc8f324c63cb359cacec8e
Stephen-Lee <stephen_leez@sina.com>
2015-03-22 18:40:24 +0800

    initialize

---
+++ db/migrate/20150322095532_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :nick_name
+      t.string :address
+      t.integer :phone
+      t.string :real_name
+
+      t.timestamps null: false
+    end
+    add_index :users, :id
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    nick_name "MyString"
+address "MyString"
+phone 1
+real_name "MyString"
+  end
+
+end
